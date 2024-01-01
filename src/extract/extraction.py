3c62ec8864a19c4ffa3abf39eadc6994a70d18ee
trihuynh0410 <trihuynh0410@gmail.com>
2024-01-01 14:56:51 +0700

    add sth

--- src/extract/extraction.py
+++ src/extract/extraction.py
@@ -10,6 +10,6 @@ warnings.filterwarnings("ignore")
 
 if __name__ == "__main__":
     region = sys.argv[1] # region = hippo or ven
-    for folder in  ["CN", "MCI", "Mild", 'Mod']:
+    for folder in  ['Mod']:
         process_subjects_in_folder(f'data/ADNI_out_mgz/{folder}', region) 
         print(f"Done {folder}")
